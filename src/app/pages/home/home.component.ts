43addc5bfba94aa0226cb8543043ead67334cba4
Amilcar Joao <ajeyslee@gmail.com>
2025-05-30 04:38:37 +0200

    Page Home bien avancée - footer modifié

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,31 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PlayerSoundtracksComponent } from "../../widgets/player-soundtracks/player-soundtracks.component";
 import { PLAYER_CONFIGS } from '../../configs/player-configs';
+import { AudioService } from '../../services/audio.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PlayerSoundtracksComponent],
+  imports: [CommonModule, PlayerSoundtracksComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
   protected PLAYER_CONFIGS = PLAYER_CONFIGS;
+  
+  // Variables pour le mini-player des témoignages
+  isPlayingTestimonial: boolean = false;
+  currentTestimonial: string = '';
+  testimonialAudio: HTMLAudioElement | null = null;
+  
+  // Subscription pour écouter les événements du service audio
+  private audioSubscription: Subscription = new Subscription();
+  
+  constructor(private audioService: AudioService) {}
   
   // NE PAS TOUCHER CE CODE - C'EST POUR LA CAROUSEL
   @ViewChild('playersWrapper') playersWrapper!: ElementRef;
@@ -62,5 +75,71 @@ export class HomeComponent {
     });
   }
 
-//FIN DE CLASS DONT TUCH
+  // Fonction pour gérer la lecture audio des témoignages
+  togglePlayTestimonial(id: string) {
+    // Arrêter tous les autres lecteurs audio via le service
+    this.audioService.stopAllAudio();
+    
+    // Si c'est le même audio, basculer entre lecture et pause
+    if (this.currentTestimonial === id && this.testimonialAudio) {
+      if (this.isPlayingTestimonial) {
+        this.testimonialAudio.pause();
+        this.isPlayingTestimonial = false;
+        this.audioService.clearActivePlayer();
+      } else {
+        this.testimonialAudio.play();
+        this.isPlayingTestimonial = true;
+        this.audioService.setActivePlayer('testimonial-' + id);
+      }
+      return;
+    }
+    
+    // Si un autre audio testimonial est en cours de lecture, l'arrêter
+    if (this.testimonialAudio) {
+      this.testimonialAudio.pause();
+      this.testimonialAudio = null;
+      this.isPlayingTestimonial = false;
+    }
+    
+    // Si c'est un nouvel audio, le créer et le lire
+    this.currentTestimonial = id;
+    
+    // URL de l'audio en fonction de l'ID
+    let audioUrl = '';
+    if (id === 'kia') {
+      audioUrl = 'https://audio-applisound.s3.eu-west-3.amazonaws.com/soundtracks_homePage/Jeyslee+-+Build+Your+Dreams+2.wav';
+    }
+    
+    if (audioUrl) {
+      this.testimonialAudio = new Audio(audioUrl);
+      this.testimonialAudio.addEventListener('ended', () => {
+        this.isPlayingTestimonial = false;
+        this.audioService.clearActivePlayer();
+      });
+      this.testimonialAudio.play();
+      this.isPlayingTestimonial = true;
+      this.audioService.setActivePlayer('testimonial-' + id);
+    }
+  }
+  
+  ngOnInit() {
+    // S'abonner aux événements d'arrêt de tous les lecteurs
+    this.audioSubscription = this.audioService.stopAllPlayers$.subscribe(stop => {
+      if (stop && this.testimonialAudio && this.isPlayingTestimonial) {
+        this.testimonialAudio.pause();
+        this.isPlayingTestimonial = false;
+      }
+    });
+  }
+  
+  ngOnDestroy() {
+    // Arrêter la lecture audio lors de la destruction du composant
+    if (this.testimonialAudio) {
+      this.testimonialAudio.pause();
+      this.testimonialAudio = null;
+    }
+    
+    // Se désabonner pour éviter les fuites de mémoire
+    this.audioSubscription.unsubscribe();
+  }
 }
--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -7,14 +7,35 @@ import { BehaviorSubject } from 'rxjs';
 export class AudioService {
   private activePlayerId = new BehaviorSubject<string | null>(null);
   activePlayer$ = this.activePlayerId.asObservable();
+  
+  // Ajout d'un événement pour notifier l'arrêt de tous les lecteurs
+  private stopAllPlayersSubject = new BehaviorSubject<boolean>(false);
+  stopAllPlayers$ = this.stopAllPlayersSubject.asObservable();
 
   setActivePlayer(componentId: string) {
-    if (this.activePlayerId.value !== componentId) {
+    // Si un autre lecteur est actif, on envoie d'abord un signal pour tout arrêter
+    if (this.activePlayerId.value !== null && this.activePlayerId.value !== componentId) {
+      this.stopAllPlayersSubject.next(true);
+      // Petit délai pour laisser le temps aux lecteurs de s'arrêter
+      setTimeout(() => {
+        this.activePlayerId.next(componentId);
+      }, 50);
+    } else {
       this.activePlayerId.next(componentId);
     }
   }
 
   getActivePlayer(): string | null {
     return this.activePlayerId.value;
+  }
+  
+  clearActivePlayer() {
+    this.activePlayerId.next(null);
+  }
+  
+  // Méthode pour arrêter tous les lecteurs audio
+  stopAllAudio() {
+    this.stopAllPlayersSubject.next(true);
+    this.activePlayerId.next(null);
   }
 }
