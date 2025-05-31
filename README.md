652f570621fe2010ee20982461c5ac5b487c10e1
Amilcar Joao <ajeyslee@gmail.com>
2025-05-31 17:38:57 +0200

    Problèmes de langue réglés V2 - OK OK VRM

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 Application web pour services de bandes sonores
 
 ## Changé de dossier grand projet APPLISOUND
+
+
+## Mes notes personnelles
+
+Je viens enfin de régler la plus part des problématiques de traduction. 
--- src/app/page-elements/footer/footer.component.ts
+++ src/app/page-elements/footer/footer.component.ts
@@ -8,28 +8,40 @@ import { TranslateModule } from '@ngx-translate/core';
   standalone: true,
   imports: [CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent implements OnInit {
   currentYear: number = new Date().getFullYear();
   currentLanguage: string = 'en';
-  
-  constructor(
-    public languageService: LanguageService
-  ) {}
-  
+
+  constructor(public languageService: LanguageService) {}
+
   ngOnInit() {
     // S'abonner aux changements de langue
-    this.languageService.currentLanguage$.subscribe(lang => {
+    this.languageService.currentLanguage$.subscribe((lang) => {
       this.currentLanguage = lang;
     });
   }
-  
+
   switchLang(event: Event) {
-    const select = event.target as HTMLSelectElement;
-    const language = select.value;
-    
-    // Utiliser le service de langue pour changer la langue
-    this.languageService.setLanguage(language);
+  const select = event.target as HTMLSelectElement;
+  const language = select.value;
+  
+  // Si on est à la racine et qu'on choisit une langue autre que l'anglais
+  if (language !== 'en' && window.location.pathname === '/') {
+    // Redirection avec la langue dans l'URL pour forcer un rafraîchissement complet
+    window.location.href = `/${language}`;
+    return;
   }
+  
+  // Si on est sur une page avec une langue et qu'on choisit l'anglais
+  if (language === 'en' && window.location.pathname.match(/^\/[a-z]{2}(-[a-z]{2})?$/)) {
+    // Redirection vers la racine pour forcer un rafraîchissement complet
+    window.location.href = '/';
+    return;
+  }
+  
+  // Dans les autres cas, utiliser le service de langue standard
+  this.languageService.setLanguage(language);
+}
 }
